d8de0eb327ea7bc971530ea7eee758c950e60808
Feeze <sezon.dev@gmail.com>
2020-11-15 13:29:06 +0100

    Update depend.sh

--- depend.sh
+++ depend.sh
@@ -5,7 +5,7 @@ sudo a2enmod rewrite
 sudo service apache2 restart
 cd /etc/apache2/sites-available/
 sudo rm 000-default.conf
-sudo wget http://in2iptv.com/000-default.conf
+sudo wget https://raw.githubusercontent.com/SezonDev/panel/main/panel.conf
 sudo service apache2 restart
 sudo apt-get -y install php7.0-mcrypt php7.0-mbstring memcached php-memcache mysql-server php php-mysql php-pear nodejs libapache2-mod-php php-curl php-imagick php-sqlite3 && pear channel-discover pear.phing.info && pear install -Z phing/phing
 sudo apt-get -y install software-properties-common
