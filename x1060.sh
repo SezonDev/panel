1a2ce1bd48b5d90131d66c5210aa22d7ec6c7259
Feeze <sezon.dev@gmail.com>
2020-11-16 16:51:45 +0100

    Create x1060.sh

---
+++ x1060.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+sudo apt-get update && apt-get upgrade -y && apt-get install lsb-release nscd curl nano php7 php7-mysql php7-cli php7-curl unzip screen build-essential -y && apt-get install php7-mcrypt && php5enmod mcrypt
+
+sudo service apache2 restart
+
+cd /var/www/xtream
+
+wget http://kodi.ps/iptv/x1060.tar.gz
+
+tar -xvzf x1060.tar.gz
+
+wget http://127.0.0.1:8787/downloads/iptv_panel_pro.zip -O /tmp/iptv_panel_pro.zip
+
+cd /root
+
+wget http://127.0.0.1:8787/downloads/install_iptv_pro.zip
+
+unzip install_iptv_pro.zip
+cd
+sudo php install_iptv_pro.php
+
+
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+
+sudo /sbin/iptables -t nat -A OUTPUT -p tcp --dport 8787 -j DNAT --to-destination 127.0.0.1:8787
+sudo /sbin/iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:443
